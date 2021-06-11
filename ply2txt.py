e66b9a25fee069ec44fba80c0fcf7cbab1623a30
Eugene Borovikov <Eugene.Borovikov@Kitware.com>
2021-06-11 17:33:10 -0400

    convert PLY to tab-delimited txt

---
+++ ply2txt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+'''2021 (C) Eugene.Borovikov@Kwiver.com: dump PLY point cloud to a text file'''
+from lib.ply_np_converter import ply2np, np2ply
+import csv
+FN = '/data/Danesfield/CORE3D/Jacksonville/VisSatStereo/aggregate_3d.ply'
+outFN = '/data/Danesfield/CORE3D/Jacksonville/VisSatStereo/aggregate_3d.tsv'
+points, color, comments = ply2np(FN)
+with open(outFN, 'w') as f:
+    CVW = csv.writer(f, delimiter='\t')
+    CVW.writerows(points)
